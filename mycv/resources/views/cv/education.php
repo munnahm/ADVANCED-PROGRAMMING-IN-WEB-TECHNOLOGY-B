b64ac576f1c8e469aabc6ce6dea814a559c23d7c
munnahm <munnagalaxya7@gmail.com>
2022-02-12 12:41:14 +0600

    task 2

---
+++ mycv/resources/views/cv/education.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profile</title>
+</head>
+<body>
+	<table>
+		<th>
+			EDUCATION
+		</th>
+		<th>
+			BOARD
+		</th>
+		<th>
+			INSTITUTION
+		</th>
+		<th>
+			PASSING YEAR
+		</th>
+		<th>
+			GPA
+		</th>
+		<tr>
+		<td>SSC </td> <td>Dhaka  </td> <td>Sahajuddin Sarker model school and collage </td> <td>2016 </td> <td>3.83 </td></tr>
+		<tr>
+		<td>HSC </td> <td>Dhaka  </td> <td>Sahajuddin Sarker model school and collage </td> <td>2018 </td> <td>2.83 </td></tr>
+		<tr>
+		<td>BSc CSE </td> <td>- </td> <td>merican International University-Bangladesh (AIUB) </td> <td>- </td> <td>3.61(CGPA) </td></tr>
+	</table>
+</body>
+</html>
---
+++ mycv/resources/views/cv/profile.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profile</title>
+</head>
+<body>
+	<h1>NAME: MUNNA, MAHEDI HASAN</h1>
+	<h2>ID: 18-388**-3</h2>
+	<h2>EMAIL: munnagalaxy**@gmail.com</h2>
+	<h2>CONTUCT: 019........</h2>
+</body>
+</html>
